9668f4ea61029a2482dca9b8061b763d16a735be
Gaurish Rane <megaurishrane@gmail.com>
2014-11-01 18:31:54 +0530

    Admin Components

---
+++ fuel/app/classes/controller/admin.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2014 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Admin extends Controller {
+
+    /**
+     * The basic welcome message
+     *
+     * @access  public
+     * @return  Response
+     */
+    public function action_index() {
+        $view = View::forge('admin/layout/dashboard');
+        $view->menu = View::forge('admin/layout/menu');
+        $view->container = 'this is container';
+        return $view;
+    }
+
+    /**
+     * A typical "Hello, Bob!" type example.  This uses a Presenter to
+     * show how to use them.
+     *
+     * @access  public
+     * @return  Response
+     */
+    public function action_form() {
+
+        $view = View::forge('admin/layout/dashboard');
+        $view->menu = View::forge('admin/layout/menu');
+        $view->container = View::forge('admin/form/form');
+        return $view;
+        //return Response::forge(Presenter::forge('welcome/hello'));
+    }
+
+    public function action_about() {
+        $view = View::Forge('layout/aboutus');
+        $view->header = View::Forge('layout/header');
+        $view->sidebar = View::forge('layout/sidebar-left');
+        $view->footer = View::Forge('layout/footer');
+        return $view;
+    }
+
+    public function action_contact() {
+        $view = View::Forge('layout/contactus');
+        $view->header = View::Forge('layout/header');
+        $view->sidebar = View::Forge('layout/sidebar-left');
+        $view->footer = View::Forge('layout/footer');
+        return $view;
+    }
+
+    public function action_sellrentproperty() {
+        $view = View::Forge('layout/property');
+        $view->header = View::Forge('layout/header');
+        $view->sidebar = View::Forge('layout/sidebar-left');
+        $view->form = View::Forge('layout/postproperty');
+        $propertytype = View::Forge('layout/propertytype');
+        $view->form->propertytype = $propertytype;
+        $view->footer = View::Forge('layout/footer');
+        return $view;
+    }
+
+    /**
+     * The 404 action for the application.
+     *
+     * @access  public
+     * @return  Response
+     */
+    public function action_404() {
+        return Response::forge(Presenter::forge('welcome/404'), 404);
+    }
+
+}
---
+++ fuel/app/views/admin/layout/menu.php
@@ -0,0 +1,69 @@
+ <div class="navbar-default sidebar" role="navigation">
+                    <div class="sidebar-nav navbar-collapse">
+                        <ul class="nav" id="side-menu">
+                            <li class="sidebar-search">
+                                <div class="input-group custom-search-form">
+                                    <input type="text" class="form-control" placeholder="Search...">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                                <!-- /input-group -->
+                            </li>
+                            <li>
+                                <a class="active" href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Sell Property <span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>mastercategory">Create Property</a>
+                                    </li>
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>mastercategoryt">View Sell Property</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Buy Property<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>subcategory">Create Property</a>
+                                    </li>
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>subcategoryt">View Buy Property</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Bungalow <span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>subcategory">Create Property</a>
+                                    </li>
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>subcategoryt">View Buy Property</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> User List<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>product">Create User</a>
+                                    </li>
+                                    <li>
+                                        <a href="<?php echo Uri::base(false);?>productt">View User</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /.sidebar-collapse -->
+                </div>
