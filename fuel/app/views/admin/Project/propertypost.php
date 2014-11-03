3d7d6c6b5407caca0d03fb2eec0dd7b4a3f2a9b0
jaybind <jabind10@gmail.com>
2014-11-02 23:45:40 -0800

    Project form
    Personal Detail and Amenities

--- fuel/app/views/admin/Project/propertypost.php
+++ fuel/app/views/admin/Project/propertypost.php
@@ -73,50 +73,96 @@
                     </form>
                     <!-- /.table-responsive -->
                     </div>
-         <div class="col-md-6">
-                      <label style="margin-left: 60px;"><h3><b>Project Amenities</b></h3></label>
-                    <form role="form">
-                        <div class="form-group" >
+                    <div class="col-md-6">
+                        <label style="margin-left: 60px;"><h3><b>Project Amenities</b></h3></label>
+                        <form role="form">
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Security Guard</b></label>
+
+                                <label><input type="checkbox" name="securityguard" value="securityguard"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b> Swimming pull </b></label>
+                                <label><input type="checkbox" name="swimmingpull" value="swimmingpull"></label>
+
+                            </div>
+
+                            <div class="form-group" >
+                                <label style="margin-left: 50px;"><b>GYM </b></label>
+                                <label><input type="checkbox" name="gym" value="gym"></label>
+                            </div>
+                            <div class="form-group" >
+                                <label style="margin-left: 50px;"><b>WIFI </b></label>
+                                <label><input type="checkbox" name="wifi" value="wifi"></label>
+                            </div>
+
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 50px;"><b>Mandir</b></label>
+                                <label><input type="checkbox" name="madir" value="mandir"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Kids Play Area</b></label>
+                                <label><input type="checkbox" name="kidsplayarea" value="kidsplayarea"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Jogging Track</b></label>
+                                <label><input type="checkbox" name="joggingtrack" value="joggingtrack"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Indoor Games</b></label>
+                                <label><input type="checkbox" name="indoorgame" value="indoorgame"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Terrace Garden</b></label>
+                                <label><input type="checkbox" name="terracegarden" value="terracegarden"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b>Internal Raod</b></label>
+                                <label><input type="checkbox" name="internalroad" value="internalroad"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b> CCTV Camera </b></label>
+                                <label><input type="checkbox" name="cctv" value="cctv"></label>
+                            </div>
+                            <div class="form-group" >
+
+
+                                <label style="margin-left: 40px;"><b> Community hall </b></label>
+                                <label><input type="checkbox" name="communityhall" value="communityhall"></label>
+                            </div>
+                            <div class="form-group">
+                                <label> Amenities Images</label>
+                                <input type="file" multiple name="AminitiesImages" class="form-control" value="" required>
+                                <span id="validation" style="color: red"></span>
+                            </div>   
                             
-                            <div class="radio">
-                                <label><b>Security Guard</b></label>
+                           
+                            <div class="checkbox">
                                 <label>
-                                    <input type="radio" name="security" id="optionsRadios1" value="Yes" checked>Yes
-                                </label>
-                                <label>
-                                    <input type="radio" name="security" id="optionsRadios1" value="No" >No
+                                    <input type="checkbox" name="check"value="check"> Every thing is OK
                                 </label>
                             </div>
-                        </div>
-                         <div class="form-group" >
-                            
-                            <div class="radio">
-                                <label><b>Security Guard</b></label>
-                                <label>
-                                    <input type="radio" name="security" id="optionsRadios1" value="Yes" checked>Yes
-                                </label>
-                                <label>
-                                    <input type="radio" name="security" id="optionsRadios1" value="No" >No
-                                </label>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputFile">File input</label>
-                            <input type="file" id="exampleInputFile">
-                            <p class="help-block">Example block-level help text here.</p>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox"> Check me out
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
-                </div>
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    </div>
                     </div>
                     <!-- /.row -->
                     </div>
