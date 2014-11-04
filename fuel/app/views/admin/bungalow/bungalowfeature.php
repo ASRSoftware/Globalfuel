5ae24122658b342ca6e212a101bda1fb4c2b0736
jaybind <jabind10@gmail.com>
2014-11-04 12:01:31 +0530

    Revert "Create user form is created successfully"
    This reverts commit 5d2590a927ef3ad07eca486607d74d11e25ed68d.

--- fuel/app/views/admin/bungalow/bungalowfeature.php
+++
@@ -1,5 +0,0 @@
-<legend>Bungalow Details</legend>
-<div class="form-group">
-    <label for="bungalowname">Bungalow Name* :</label>
-    <input type="text" class="form-control" id="exampleInputtext" placeholder="Bungalow name" name="bunglowname" required>
-</div>
